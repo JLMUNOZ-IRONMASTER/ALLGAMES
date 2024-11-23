3347d02bd8de9e733b03f430390a9c4a158d02c0
Jose Luis Ernesto Muñoz Rubio <Jose Luis Munoz@CLSANHQ25PDR73.fss.aramark.com>
2024-11-23 17:44:54 -0300

    ALL GAMES PROYECT

---
+++ App.js
@@ -0,0 +1,48 @@
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+import { StatusBar } from 'expo-status-bar';
+import {useEffect,useState} from 'react'
+
+//import TabNavigator from './src/navigation/TabNavigator';
+import MainNavigator from './src/navigation/MainNavigator';
+
+import { store } from './src/app/store';
+import { Provider } from 'react-redux';
+
+import { createSessionsTable } from './src/db';
+
+//import { clearSessions } from './src/db';
+
+createSessionsTable()
+  .then((result)=>console.log("Tabla creada o inicializada con éxito: ", result))
+  .catch((error)=>console.log("Error al crear la tabla Sessions: ", error))
+
+  //clearSessions().then().catch()
+
+SplashScreen.preventAutoHideAsync();
+
+
+export default function App() {
+  const [loaded, error] = useFonts({
+    'Montserrat': require('./assets/fonts/Montserrat-Variable.ttf'),
+    'airstrike': require('./assets/fonts/airstrike.ttf')
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <Provider store={store}>
+      <MainNavigator />
+      <StatusBar style="light" />
+    </Provider>
+  );
+}
